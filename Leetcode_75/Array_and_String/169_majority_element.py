fa98690f25d55b75024b25e278857fa8709c23a5
0921sean <0921sean@gmail.com>
2025-04-09 18:39:46 +0900

    [04.09]

---
+++ Leetcode_75/Array_and_String/169_majority_element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        element_count = {}
+        for num in nums:
+            element_count[num] = element_count.get(num, 0) + 1
+
+        for element, count in element_count.items():
+            if count > len(nums) / 2:
+                return element
